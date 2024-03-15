01c346408e3cb8024cb259d200278dac306887dc
SGwachha <gwachhas123@gmail.com>
2024-03-15 09:56:13 +0545

    code completion need to fix firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import ChatBox from "./component/ChatBox";
+import Welcome from "./component/Welcome";
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
+      <Navbar />
+      {!user ? (
+        <Welcome />
+      ) : (
+        <>
+          <ChatBox />
+        </>
+      )}
     </div>
   );
 }
